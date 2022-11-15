965248f446102a8f718d5a1e998821effd591a69
Rita <ritaisabelmq@gmail.com>
2022-11-15 10:15:17 +0000

    node.js connection files

---
+++ node.js/connection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect();
+
+function queryDB(query, valores) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, valores, function (err, resultados) {
+            if (err)
+                reject(err);
+            else
+                resolve(resultados);
+        })
+    })
+}
+
+module.exports = {queryDB, connection};
